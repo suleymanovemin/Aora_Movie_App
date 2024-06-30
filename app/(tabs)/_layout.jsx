8ff3677e45613bbff14c3eead7d510a5ad7ede4a
Emin Suleymanov <emin.suleymanov2001@gmail.com>
2024-06-30 12:03:51 +0400

    first commit

---
+++ app/(tabs)/_layout.jsx
@@ -0,0 +1,104 @@
+import { View, Text, Image } from "react-native";
+import React from "react";
+import { Tabs } from "expo-router";
+import { icons } from "../../constants";
+const TabIcon = ({ icon, color, name, focused }) => {
+  return (
+    <View className="items-center justify-center gap-2">
+      <Image
+        source={icon}
+        resizeMode="contain"
+        tintColor={color}
+        className="w-6 h-6"
+      />
+      <Text
+        className={`${focused ? "font-psemibold" : "font-pregular"} text-xs`}
+        style={{ color }}
+      >
+        {name}
+      </Text>
+    </View>
+  );
+};
+const TabsLayout = () => {
+  return (
+    <>
+      <Tabs
+        screenOptions={{
+          tabBarShowLabel: false,
+          tabBarActiveTintColor: "#FFA001",
+          tabBarInactiveTintColor: "#CDCDE0",
+          tabBarStyle: {
+            backgroundColor: "#161622",
+            borderTopWidth: 1,
+            borderTopColor: "#232533",
+            height: 84,
+          },
+        }}
+      >
+        <Tabs.Screen
+          name="home"
+          options={{
+            title: "Home",
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon={icons.home}
+                color={color}
+                focused={focused}
+                name="Home"
+              />
+            ),
+            headerShown: false,
+          }}
+        />
+        <Tabs.Screen
+          name="bookmark"
+          options={{
+            title: "Bookmark",
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon={icons.bookmark}
+                color={color}
+                name="Bookmark"
+                focused={focused}
+              />
+            ),
+            headerShown: false,
+          }}
+        />
+        <Tabs.Screen
+          name="create"
+          options={{
+            title: "Create",
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon={icons.plus}
+                color={color}
+                focused={focused}
+                name="Create"
+              />
+            ),
+            headerShown: false,
+          }}
+        />
+        <Tabs.Screen
+          name="profile"
+          options={{
+            title: "Profile",
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon={icons.profile}
+                color={color}
+                focused={focused}
+                name="Profile"
+              />
+            ),
+            headerShown: false,
+          }}
+        />
+      </Tabs>
+    </>
+  );
+};
+
+export default TabsLayout;
---
+++ components/FormField.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import { View, Text, TextInput, TouchableOpacity, Image } from "react-native";
+import { icons } from "../constants";
+
+const FormField = ({
+  title,
+  value,
+  handleChangeText,
+  otherStyles,
+  keyboardType,
+  placeholder,
+  ...props
+}) => {
+  const [showPassword, setShowPassword] = useState(false);
+  return (
+    <View className={`space-y-2 ${otherStyles}`}>
+      <Text className="text-base text-gray-100 font-pmedium">{title}</Text>
+      <View className="w-full h-16 px-4 bg-black-200 border-2 border-black-100 rounded-2xl focus:border-secondary items-center flex-row">
+        <TextInput
+          className="flex-1 text-white font-psemibold text-base"
+          value={value}
+          onChangeText={handleChangeText}
+          keyboardType={keyboardType}
+          placeholder={placeholder}
+          placeholderTextColor="#7b7b8b"
+          secureTextEntry={title === "Password" && !showPassword}
+        />
+        {title === "Password" && (
+          <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
+            <Image
+              source={!showPassword ? icons.eye : icons.eyeHide}
+              className="w-6 h-6 "
+              resizeMode="contain"
+            />
+          </TouchableOpacity>
+        )}
+      </View>
+    </View>
+  );
+};
+
+export default FormField;
